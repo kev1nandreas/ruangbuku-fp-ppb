adb3510f74776ed85740abfa111a9557bf8ef727
kev1nandreas <kevin.andreascn@gmail.com>
2026-05-29 22:27:21 +0700

    feat: implement response structure in Controller and add api routes

--- backend/app/Http/Controllers/Controller.php
+++ backend/app/Http/Controllers/Controller.php
@@ -2,7 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\JsonResponse;
+
 abstract class Controller
 {
-    //
+    protected function success(string $message, mixed $data = [], int $status = 200): JsonResponse
+    {
+        return response()->json([
+            'status'  => true,
+            'message' => $message,
+            'data'    => $data,
+        ], $status);
+    }
+
+    protected function error(string $message, mixed $error = [], int $status = 400): JsonResponse
+    {
+        return response()->json([
+            'status'  => false,
+            'message' => $message,
+            'error'    => $error,
+        ], $status);
+    }
+
+    protected function created(string $message, mixed $data = []): JsonResponse
+    {
+        return response()->json([
+            'status'  => true,
+            'message' => $message,
+            'data'    => $data,
+        ], 201);
+    }
 }

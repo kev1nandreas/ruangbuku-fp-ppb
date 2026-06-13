95abf8331996c010b71c5ec1a7508f43b27507a7
Kevin Andreas <kevin.andreascn@gmail.com>
2026-06-13 20:44:40 +0700

    feat: create migration files for permissions, users, and jobs tables

--- backend/database/migrations/0001_01_01_000003_create_users_table.php
+++ backend/database/migrations/0001_01_01_000003_create_users_table.php
@@ -17,6 +17,10 @@ return new class extends Migration
             $table->string('email', 50)->unique();
             $table->string('password', 255);
             $table->string('avatar_url', 512)->nullable()->after('email');
+
+            $table->unsignedBigInteger('role_id')->nullable()->after('password');
+            $table->foreign('role_id')->references('id')->on('roles')->nullOnDelete();
+            
             $table->rememberToken();
             $table->timestamps();
         });
